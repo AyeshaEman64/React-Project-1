6b892475b776ec093abca6cb03e5edaa37d3d8e2
Your Name <adnan.ali.181.ch@gmail.com>
2025-12-19 11:47:06 +0500

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import NetflixSeries from "./components/NetflixSeries";
 import { Todo } from "./projects/Todo/Todo";
+import { ContactForm } from "./useState/ContactForm";
+import { LoginForm } from "./useState/LoginForm";
+import { RegistrationFormReact } from "./useState/ResgistrationForm";
 export const App = () => {
 
   return <>
-  <NetflixSeries />
+  {/* <NetflixSeries />
   <Todo />
+  <RegistrationFormReact/>
+  <LoginForm /> */}
+  <ContactForm />
+
   </>
   
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+// import { App } from "./App.jsx";
 import './index.css'
 
 // import Profile from "./components/Profile.jsx";
 //  import { Practice } from "./Practice.jsx";
 import "./index.css";
+import { CleanUp } from "./hooks/useEffect/cleanUp";
+// import { Challenge } from "./hooks/useEffect/effectChallange";
+// import { ReactUseEffect } from "./hooks/useEffect/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Practice /> */}
     {/* <Profile /> */}
+    {/* <ReactUseEffect/> */}
+    {/* <challenge /> */}
+    <CleanUp/>
   </React.StrictMode>
 );
